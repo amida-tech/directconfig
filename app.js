c4cd722c8ee1c4a91a7b99d0159d9c4548e09496
au2 <afsin@amida-tech.com>
2014-03-06 15:35:15 -0500

    for putcert -certificate directory from commandline -certificate filename from route

--- app.js
+++ app.js
@@ -3,15 +3,25 @@
  */
 
 var restify = require('restify');
+var path = require('path');
 
 var fileutil = require("./lib/fileutil");
+var configserver = require("./lib/configserver");
 
 var server = restify.createServer();
 
 var keyDirectory = '/opt/direct/certificates';
 var logDirectory = '/var/log/upstart/direct-config.log';
 
-server.put('/certificate', fileutil.putFile);
-server.get('/certificate', fileutil.getFile);
+server.put('/cert/:filename', function(req, res, next) {
+	var dirpath = (process.argv.length > 2) ? process.argv[2] : keyDirectory;
+	var filepath = path.join(dirpath, req.params.filename);
+	fileutil.putFile.call(this, req, filepath, function(error) {
+		configserver.putCert(filepath);
+		res.send(200);
+	});
+});
+
+server.get('/cert/:filename', fileutil.getFile);
 
 server.listen(3000);
