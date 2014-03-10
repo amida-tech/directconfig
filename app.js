c3288ce6cb617e86eff9037af2403f01f72b2d7c
au2 <afsin@amida-tech.com>
2014-03-10 11:44:13 -0400

    - private key is now created when direct server service starts

--- app.js
+++ app.js
@@ -7,31 +7,37 @@ var path = require('path');
 
 var fileutil = require("./lib/fileutil");
 var configserver = require("./lib/configserver");
+var openssl = require("./lib/opensslwrap");
 
 var server = restify.createServer();
 
-var keyDirectory = '/opt/direct/certificates';
-var logDirectory = '/var/log/upstart/direct-config.log';
-
-var options = {
+var settings = {
+	pyoptions: {
 		pyscriptsdir: path.join(__dirname, "pyscripts"),
 		pycmd: "python"
+	},
+	certDir: (process.argv.length > 2) ? process.argv[2] : '/opt/direct/certificates',
+	passout: (process.argv.length > 3) ? process.argv[3] : 'pass:""',
+	privKeyFilename: "rsa-key.pem",
+	getPrivKeyPath: function() {
+		return path.join(this.certDir, this.privKeyFilename);
+	}
 };
 
 server.put('/cert/:filename', function(req, res, next) {
-	var dirpath = (process.argv.length > 2) ? process.argv[2] : keyDirectory;
-	var filepath = path.join(dirpath, req.params.filename);
-        fileutil.putFile.call(this, req, filepath, function(error) {
-		configserver.putCert(options, filepath);
-		res.send(200);
+	var filepath = path.join(settings.certDir, req.params.filename);
+    fileutil.putFile.call(this, req, filepath, function(error) {
+        configserver.putCert(settings.pyoptions, filepath);
+        res.send(200);
 	});
 });
 
 server.del('/reset', function(req, res, next) {
-	configserver.clearAll(options);
+	configserver.clearAll(settings.pyoptions);
 	res.send(200);
 });
 
 server.get('/cert/:filename', fileutil.getFile);
 
+openssl.createPrivateKey(settings);
 server.listen(3000);
